0123b6775961dc0a1a58778ee902c7424ecf9fca
Tomáš Trungel <t.trungel@gmail.com>
2012-12-23 23:15:29 +0100

    Fixed a mistake in initial commit. Should be OK.

--- src/school/trungi/tpac/MainActivity.java
+++ src/school/trungi/tpac/MainActivity.java
@@ -3,6 +3,7 @@ package school.trungi.tpac;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Ahoj, moje nová super androidí aplikace!", Toast.LENGTH_LONG).show();
     }
 
     @Override
